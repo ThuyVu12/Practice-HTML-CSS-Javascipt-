b20494a3701cfeaee54480ee4a365833c986e6d1
Thuy Vu <106577887+ThuyVu12@users.noreply.github.com>
2023-04-13 22:12:07 -0700

    Add files via upload

---
+++ Button Clicker/script.js
@@ -0,0 +1,10 @@
+function logout() {
+    document.getElementById('login').value = 'logout';
+}
+
+
+function hide(element) {
+    console.log ("the function 2 is running");
+    element.remove();
+}
+
