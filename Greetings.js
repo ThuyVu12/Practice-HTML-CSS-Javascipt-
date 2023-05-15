3abaa0f5b3abffa6dafe85576febdcf51519dcef
Thuy Vu <106577887+ThuyVu12@users.noreply.github.com>
2023-05-15 16:56:51 -0700

    Update Greetings.js

--- Greetings.js
+++ Greetings.js
@@ -1,4 +1,5 @@
 // Write your code here:
+// using for loop
 function shoutGreetings(array) {
   let newGreetings = [];
   for (let i = 0; i < array.length; i++) {
@@ -8,6 +9,13 @@ function shoutGreetings(array) {
   return newGreetings;
 };
 
+// using .map()
+// const shoutGreetings = (arr) => {
+//   let newGreetings = arr.map(greeting => {
+//     return greeting.toUpperCase() + '!';
+//   });
+//   return newGreetings;
+// };
 
 // Feel free to uncomment out the code below to test your function!
 
