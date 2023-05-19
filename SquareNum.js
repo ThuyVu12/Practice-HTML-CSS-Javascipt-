d37aa53de6a4fd99cfa523e51e9ed96d7ae64493
Thuy Vu <106577887+ThuyVu12@users.noreply.github.com>
2023-05-18 22:10:59 -0700

    Update SquareNum.js

--- SquareNum.js
+++ SquareNum.js
@@ -1,6 +1,6 @@
-const numbers = [2, 7, 9, 171, 52, 33, 14]
+const numbers = [2, 7, 9, 171, 52, 33, 14];
 
-const toSquare = num => num * num
+const toSquare = num => num * num;
 
 // Write your code here:
 const squareNums = array => {
